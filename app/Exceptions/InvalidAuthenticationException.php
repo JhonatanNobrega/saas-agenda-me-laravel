43768bed5c46fa0cc9016640584b0b7b25509990
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2024-01-23 18:41:04 -0300

    Traits padrao para exception

--- app/Exceptions/InvalidAuthenticationException.php
+++ app/Exceptions/InvalidAuthenticationException.php
@@ -2,17 +2,19 @@
 
 namespace App\Exceptions;
 
+use App\Traits\RenderToJson;
 use Exception;
 
 class InvalidAuthenticationException extends Exception
 {
+    use RenderToJson;
     protected $message = "Your credentials don't match.";
+    protected $code = 400;
     public function render()
     {
         return response()->json([
             "error" => class_basename($this),
             "message" => $this->getMessage()
-        ], 400);
+        ], $this->getCode());
     }
-
 }
